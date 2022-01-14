3b63a1ac2f7124b05ca8d4c2b15eb902962ac0b4
Vahtee Boo <vahteeboo@gmail.com>
2022-01-14 14:05:25 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Vahtee Boo on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
